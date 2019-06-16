50604e85af04b234c2034e58e92279677bb85833
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2019-06-16 08:14:40 +0100

    Add event: arrow rain

--- Assets/Scripts/VerticalArrowSpawn.cs
+++ Assets/Scripts/VerticalArrowSpawn.cs
@@ -2,38 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArrowSpawner : MonoBehaviour
+public class VerticalArrowSpawn : MonoBehaviour
 {
-    public ArrowScript arrowPrefab;
+    public VArrowScript arrowPrefab;
     public float arrowSpeed;
     private float time = 0;
     public float arrowDelay;
 
-    public bool fromLeft = false;
+    public bool fromAbove = false;
     public bool shoot;
 
     private Vector2 dir2;
 
     void Start()
     {
-        dir2 = new Vector2(arrowSpeed, 0);
+        dir2 = new Vector2(arrowSpeed, -360);
     }
 
     void Update()
     {
-        if(shoot == true)
+        if (shoot == true)
         {
             if (time < Time.time)
             {
-                ArrowScript arrow = Instantiate(arrowPrefab, transform.position, transform.rotation) as ArrowScript;
+                VArrowScript arrow = Instantiate(arrowPrefab, transform.position, transform.rotation) as VArrowScript;
                 arrow.dir = dir2;
-                if (fromLeft == true)
+                if (fromAbove == true)
                 {
-                    arrow.transform.rotation = Quaternion.Euler(0,0,0);
-                }
-                else
-                {
-                    arrow.transform.rotation = Quaternion.Euler(0, 0, 0);
+                    arrow.transform.rotation = Quaternion.Euler(-45, -90, 90);
                 }
                 time = Time.time + arrowDelay;
             }
@@ -42,7 +38,7 @@ public class ArrowSpawner : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
             shoot = true;
         }
